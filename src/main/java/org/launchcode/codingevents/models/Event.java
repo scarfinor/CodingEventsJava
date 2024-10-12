806f1dda8b7fa91b11ddd7ae56783375b64e7dc1
scarfinor <scarfinor@gmail.com>
2024-10-11 21:00:12 -0500

    new branch my-views-exercises-solution add class Event.java create.html allows name description and image EventController.java updated to take hashmaps instead of list bootstrap style sheet used model to display events in view

---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,34 @@
+package org.launchcode.codingevents.models;
+
+public class Event {
+
+    private String name;
+    private String description;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
