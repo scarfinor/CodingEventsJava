f0c8dc41e16ba4e943cf40642daa4ce282529142
scarfinor <scarfinor@gmail.com>
2024-10-26 23:20:54 -0500

    added EventCategoryRepository.java EventCategoryController.java eventCategory templates(index.html/create.html)
    updated
    EventController.java
    fragments.html

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -16,6 +16,7 @@ import org.springframework.validation.Errors;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
     @Autowired
     private EventRepository eventRepository;
 
--- src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -1,10 +1,9 @@
 package org.launchcode.codingevents.data;
 
-import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends CrudRepository<Event, Integer> {
+public interface EventCategoryRepository extends CrudRepository<EventCategory, Integer> {
 }
-
