2ce0313eb26c23dcfe1b5bea30879ab1faed0e18
scarfinor <scarfinor@gmail.com>
2024-11-06 00:24:06 -0600

    updated application.properties (database user login) User.java (encoder field/constructor/isMatchingPassword())
    added
    AuthenticationController.java (userSessionKey field, getUserFormSession(), setUserInSession(), displayRegistrationForm(), processRegistrationForm(), displayLoginForm, logout())
    login.html (view for logging in)
    register.html (view for  User registration)
    LoginFormDTO (user field, password field, getters/setters)
    RegisterFormDTO.java (verifyPassword field, getter/setter)

--- src/main/java/org/launchcode/codingevents/models/User.java
+++ src/main/java/org/launchcode/codingevents/models/User.java
@@ -7,19 +7,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Entity
 public class User extends AbstractEntity {
 
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     @NotNull
     private String username;
 
     @NotNull
     private String pwHash;
 
-    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
     public User() {}
 
     public User(String username, String password) {
         this.username = username;
-        this.pwHash = password;
+        this.pwHash = encoder.encode(password);
     }
 
     public String getUsername() {
